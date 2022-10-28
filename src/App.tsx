c514d1e16a7fad8148b2f69824f261773044256a
Joao Dias <jpgd94@poli.ufrj.br>
2022-10-28 13:02:11 -0300

    Refactor:"Usando Eslint Formatter"

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from './components/Button';
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
-import { GlobalStyle } from "./styles/global"; // Importando estilo global
-import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from './styles/global' // Importando estilo global
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-
     <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-      <GlobalStyle /> 
+      <GlobalStyle />
       {/* Importando estilo global */}
     </ThemeProvider>
   )
 }
-
