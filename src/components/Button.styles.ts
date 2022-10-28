f30ed8dd35d6dfdbdce644f7fd8795b60039051e
Joao Dias <jpgd94@poli.ufrj.br>
2022-10-28 11:08:21 -0300

    "estruturando as cores e fontes"

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -15,6 +15,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border-radius: 4px;
   border: 0;
   margin: 8px;
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme["green-500"]};
   color: ${props => props.theme.white};
   `;
