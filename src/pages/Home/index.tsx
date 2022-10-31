4ef374aa2a8ea594982ecc808dbd7664e3aec6d5
Joao Dias <jpgd94@poli.ufrj.br>
2022-10-31 16:03:16 -0300

    feat:using hookform,  register, handleSubmit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from 'phosphor-react'
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 import {
   CountdownContainer,
@@ -11,26 +11,35 @@ import {
   TaskInput,
 } from './styles'
 
-// controlled(manter em tempo real o estado, valor atualizado do estado) - usando "Onchange" / uncontrolled
+// controlled(manter em tempo real o estado, valor atualizado do estado) - usando "Onchange", usado para formulatios simples
+// uncontrolled
 
 export function Home() {
-  const [task, setTask] = useState('')
+  const { register, handleSubmit, watch } = useForm() // useForm retorna um objeto com varias funções e variaveis.
+  // register vai adicionar um input ao formulário. register recebe o nome do input e retorna metodos para trabalhar com inputs. ex: Onchange, onFocus, etc.
+  // handleSubmit vai gatilhar quando o formula´rio for submetido
+  // watch vai monitorar o campo "task"
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisable = !task // Vai estar desabilitado quando o campo "task" estiver vazio
 
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
-            name="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
-            onChange={(e) => setTask(e.target.value)}
-            value={task}
+            {...register('task')} // spreadoperator devolver os atributos que o o "register" já traz como herança. Tudo que o register já tem dentro dele
+            // lista de opções de task, sugestões, como se fosse um campo Choices. A função recebe um nome, esse nome é o "name" do input criado pela própia "register"
           />
 
-          {/* // lista de opções de task, sugestões, como se fosse um campo Choices */}
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
             <option value="Projeto 2" />
@@ -46,6 +55,7 @@ export function Home() {
             step={5} // Aumenta de 5 em os segundos
             min={5} // valor minimo
             max={60} // valor máximo
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -59,7 +69,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={!task} type="submit"> {/*desabilitando quando a task está vazia*/}
+        <StartCountdownButton disabled={isSubmitDisable} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
